4113486cc255061e352db5b3b7063479c2b4f5d0
BarryDong <286084845@qq.com>
2026-03-11 23:01:43 +0800

    style: 引入 normalize.css 统一浏览器默认样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import 'normalize.css'
 import './index.scss'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
