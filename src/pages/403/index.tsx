25238718863504a434bc176c9b38a721ae304ed1
BarryDong <286084845@qq.com>
2026-03-13 16:42:41 +0800

    style: 403、404 页面样式优化

--- src/pages/403/index.tsx
+++ src/pages/403/index.tsx
@@ -2,26 +2,30 @@ import { Button, Result, Space } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
+import './index.scss'
+
 function Forbidden() {
   const { t } = useTranslation()
   const navigate = useNavigate()
 
   return (
-    <Result
-      status="403"
-      title="403"
-      subTitle={t('exception.forbidden')}
-      extra={(
-        <Space>
-          <Button type="primary" onClick={() => navigate('/dashboard')}>
-            {t('common.backHome')}
-          </Button>
-          <Button onClick={() => navigate(-1)}>
-            {t('common.backPrevious')}
-          </Button>
-        </Space>
-      )}
-    />
+    <div className="forbidden-page">
+      <Result
+        status="403"
+        title="403"
+        subTitle={t('exception.forbidden')}
+        extra={(
+          <Space>
+            <Button type="primary" onClick={() => navigate('/dashboard')}>
+              {t('common.backHome')}
+            </Button>
+            <Button onClick={() => navigate(-1)}>
+              {t('common.backPrevious')}
+            </Button>
+          </Space>
+        )}
+      />
+    </div>
   )
 }
 
